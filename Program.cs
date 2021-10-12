3ee16f5bbde8f43f2948c08eddb6e82688f855f9
aamochalov <63183573+aamochalov@users.noreply.github.com>
2021-10-12 23:10:50 +0500

    New string about weather

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hse_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Sasha Pestova");
+            Console.WriteLine("Nice to meet you :)");
+            Console.WriteLine("alo alo, Marina will go for a walk?))");
+            Console.WriteLine("Yaaasssss!!");
+            Console.WriteLine("It's a good weather today...");
+        }
+    }
+}
