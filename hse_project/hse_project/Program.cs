932b706d577a2269c30aac71f53308b1774ee09f
PestovaAlexandra <63185698+PestovaAlexandra@users.noreply.github.com>
2021-10-13 13:51:01 +0500

    Func sum deleted

--- hse_project/hse_project/Program.cs
+++ hse_project/hse_project/Program.cs
@@ -8,15 +8,11 @@ namespace hse_project
 {
     class Program
     {
-        public static int sum (int a, int b)
-            {
-                return a+b;
-            }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, my name is Sasha Pestova");
-            Console.WriteLine(sum(5, 3));
-
+            
             Console.ReadKey();
         }
     }
